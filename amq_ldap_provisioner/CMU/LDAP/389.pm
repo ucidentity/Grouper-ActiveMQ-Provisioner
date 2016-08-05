ec35a654dbe32948394ed6d70c5f9b5258bdf892
Jeff McCullough <jeffmc@berkeley.edu>
2016-08-04 18:32:17 -0700

    changed objectClass=cmuAccoutPerson to person

--- amq_ldap_provisioner/CMU/LDAP/389.pm
+++ amq_ldap_provisioner/CMU/LDAP/389.pm
@@ -504,7 +504,7 @@ sub getUidByIsMemberOf {
 		$result = $self->ldapSearch(
 			"&(berkeleyEduIsMemberOf="
 			  . escape_filter_value($groupdn)
-			  . ")(objectClass=cmuAccountPerson)",
+			  . ")(objectClass=person)",
 			\@attrs, $self->{_peoplebase}
 		);
 
